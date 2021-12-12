706d4c3dc09163a2013b5467b0fc9a760f41b562
TheMemeMeister <60407375+TheMemeMeister@users.noreply.github.com>
2021-12-12 14:51:45 -0500

    [Modified] NetworkedClient.cs
    Changed to my local IP address for debugging

--- Assets/NetworkedClient.cs
+++ Assets/NetworkedClient.cs
@@ -85,7 +85,7 @@ public class NetworkedClient : MonoBehaviour
             hostID = NetworkTransport.AddHost(topology, 0);
             Debug.Log("Socket open.  Host ID = " + hostID);
 
-            connectionID = NetworkTransport.Connect(hostID, "192.168.43.103", socketPort, 0, out error); // server is local on network
+            connectionID = NetworkTransport.Connect(hostID, "192.168.2.39", socketPort, 0, out error); // server is local on network
 
             if (error == 0)
             {
